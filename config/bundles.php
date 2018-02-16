c14c52549710c08d7aa666b03cbbb0f5b8992d37
jgawlik <justyna.gawlik@yahoo.pl>
2018-02-16 23:28:54 +0100

    Dodanie i konfiguracja paczek umożliwiających stworzenie dokumentacji REST API

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
 ];
